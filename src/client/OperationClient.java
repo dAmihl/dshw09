9c4de26d143985d420fbb30342f123ce282d12ca
Gogeta05 <martin.agreiter@student.uibk.ac.at>
2016-05-22 05:34:47 +0200

    Time Measuring Aufgabe 1d, Kommentierung wsdl_copy.xml anhand mult Methode, Anpassung Word-Dokumente fuer Abgabe

--- src/client/OperationClient.java
+++ src/client/OperationClient.java
@@ -28,12 +28,29 @@ public class OperationClient {
         Service service = Service.create(url, qname);
 
         IWSOperations ops = service.getPort(IWSOperations.class);
+        long timeBefore = 0;
+        long timeAfter = 0;
+        
         System.out.println("Set webservice to sleep for 2 seconds first.");
         ops.sleep(5000);
-        System.out.println("Now Adding 1 and 5 on server: ");
+        System.out.println("Now Adding 1 and 5 on server:");
+        timeBefore = System.nanoTime();
         System.out.println(ops.add(1, 5));
+        timeAfter = System.nanoTime();
+        System.out.println("Time difference: " + (timeAfter - timeBefore) + " nano seconds");
         System.out.println("Now computing lucas number of 10:");
         System.out.println(ops.lucas(10));
+        System.out.println("Now multiplay 6 and 7 on server:");
+        System.out.println(ops.mult(6, 7));
+        System.out.println("Last, but not least, substract 9 from 53:");
+        System.out.println(ops.subtract(53, 9));
+        System.out.println("------------------------");
+        System.out.println("Addition by direct call to a member function:");
+        Integer result;
+        timeBefore = System.nanoTime();
+        result = 1 + 5;
+        timeAfter = System.nanoTime();
+        System.out.println("Time difference: " + (timeAfter - timeBefore) + " nano seconds");
 	}
 	
 }
